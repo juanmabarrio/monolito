8573ae1170bec5100cef780224d3df3ebf8b394f
Juanmeta <juanmabarrio@gmail.com>
2020-09-22 18:39:10 +0200

    Moving the monolito to its own directory

--- src/main/java/es/urjc/code/monolito/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package es.urjc.code.monolito.repository;
-
-import es.urjc.code.monolito.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-
-}
